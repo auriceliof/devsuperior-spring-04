e97889e87dd3ee028ce1cc6c159c76d05c1af789
auriceliof <auricelio.suporte@hotmail.com>
2023-02-26 20:46:30 -0300

    04-09 Section

--- dslearn/backend/src/main/resources/data.sql
+++ dslearn/backend/src/main/resources/data.sql
@@ -22,7 +22,9 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://www.foryou.com.br/imagemsite/2030/capa-curso.jpg', 2, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://www.foryou.com.br/imagemsite/2030/capa-curso.jpg', 0, 1);
 
-
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'https://www.foryou.com.br/imagemsite/2030/capa-curso.jpg', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos continuar', 2, 'https://www.foryou.com.br/imagemsite/2030/capa-curso.jpg', 1, 1);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos finalizar', 3, 'https://www.foryou.com.br/imagemsite/2030/capa-curso.jpg', 1, 2);
 
 
 

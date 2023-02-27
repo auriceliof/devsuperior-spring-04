093f20cdc44e600e6c4fcef18b42c6cf175ad31e
auriceliof <auricelio.suporte@hotmail.com>
2023-02-26 21:47:37 -0300

    Notification (personal)

--- dslearn/backend/src/main/resources/data.sql
+++ dslearn/backend/src/main/resources/data.sql
@@ -13,6 +13,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Notificação teste', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', false, 'Rota teste', 1);
+
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://www.foryou.com.br/imagemsite/2030/capa-curso.jpg', 'https://www.publicdomainpictures.net/pictures/190000/velka/freshman-class.jpg');
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);

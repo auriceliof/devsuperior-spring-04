b4decd6e9e9ad25c6807c67027a6f2f1730ba86a
auriceliof <auricelio.suporte@hotmail.com>
2023-06-22 21:16:12 -0300

    04-20 Incluindo exceções, validação e segurança ao projeto

--- bds-dslearn/backend/src/main/java/com/devsuperior/dslearnbds/components/JwtTokenEnhancer.java
+++ bds-dslearn/backend/src/main/java/com/devsuperior/dslearnbds/components/JwtTokenEnhancer.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dscatalog.components;
+package com.devsuperior.dslearnbds.components;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,8 +9,9 @@ import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.stereotype.Component;
-import com.devsuperior.dscatalog.entities.User;
-import com.devsuperior.dscatalog.repositories.UserRepository;
+
+import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.repositories.UserRepository;
 
 @Component
 public class JwtTokenEnhancer implements TokenEnhancer {
@@ -24,7 +25,6 @@ public class JwtTokenEnhancer implements TokenEnhancer {
 		User user = userRepository.findByEmail(authentication.getName());
 		
 		Map<String, Object> map = new HashMap<>();
-		map.put("userFirstName", user.getFirstName());
 		map.put("userId", user.getId());
 
 		DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
